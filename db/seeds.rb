6e3dea655fdf6a02ca8c9fc862b4d92d9de36168
Wei <zhangweicoding@gmail.com>
2017-05-20 14:35:20 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,48 @@
 
 flights = Flight.create([{ flight: 'GD2501',\
 	                       schedule: "08:00",\
-	                       from: 'Xi\'an' ,\
-	                       to: 'Chengdu',\
+	                       from_addr: 'Xi\'an' ,\
+	                       to_addr: 'Chengdu',\
 	                       weekdays_reg_price: 1100 ,\
 	                       weekdays_rew_price: 800 ,\
 	                       weekends_reg_price: 900 ,\
 	                       weekends_rew_price: 500 },\
 	                       { flight: 'GD2502',\
 	                       schedule: "12:00",\
-	                       from: 'Chengdu' ,\
-	                       to: 'Xi\'an',\
+	                       from_addr: 'Chengdu' ,\
+	                       to_addr: 'Xi\'an',\
 	                       weekdays_reg_price: 1700 ,\
 	                       weekdays_rew_price: 900 ,\
 	                       weekends_reg_price: 800 ,\
 	                       weekends_rew_price: 900 },\
 	                       { flight: 'GD2606',\
 	                       schedule: "12:25",\
-	                       from: 'Xi\'an' ,\
-	                       to: 'Chengdu',\
+	                       from_addr: 'Xi\'an' ,\
+	                       to_addr: 'Chengdu',\
 	                       weekdays_reg_price: 1600 ,\
 	                       weekdays_rew_price: 1100 ,\
 	                       weekends_reg_price: 600 ,\
 	                       weekends_rew_price: 400 },\
 	                       { flight: 'GD2607',\
 	                       schedule: "16:25",\
-	                       from: 'Chengdu' ,\
-	                       to: 'Xi\'an',\
+	                       from_addr: 'Chengdu' ,\
+	                       to_addr: 'Xi\'an',\
 	                       weekdays_reg_price: 1600 ,\
 	                       weekdays_rew_price: 1100 ,\
 	                       weekends_reg_price: 600 ,\
 	                       weekends_rew_price: 500 },\
 	                       { flight: 'GD8732',\
 	                       schedule: "19:30",\
-	                       from: 'Xi\'an' ,\
-	                       to: 'Chengdu',\
+	                       from_addr: 'Xi\'an' ,\
+	                       to_addr: 'Chengdu',\
 	                       weekdays_reg_price: 2200 ,\
 	                       weekdays_rew_price: 1000 ,\
 	                       weekends_reg_price: 1500 ,\
 	                       weekends_rew_price: 400 },\
 	                       { flight: 'GD8733',\
 	                       schedule: "23:30",\
-	                       from: 'Chengdu' ,\
-	                       to: 'Xi\'an',\
+	                       from_addr: 'Chengdu' ,\
+	                       to_addr: 'Xi\'an',\
 	                       weekdays_reg_price: 1600 ,\
 	                       weekdays_rew_price: 1500 ,\
 	                       weekends_reg_price: 1000 ,\
